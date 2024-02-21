4eda4905cf7de311694010c6ca21fef6629dbf11
IvanTushynski <tushinski.ivan@gmail.com>
2024-02-21 13:52:16 +0300

    update

--- src/main/java/org/teaderon/JoinListener.java
+++ src/main/java/org/teaderon/JoinListener.java
@@ -23,7 +23,7 @@ public class JoinListener implements Listener {
         p.sendMessage(ChatColor.WHITE + "---------" + ChatColor.BLUE + ChatColor.DARK_RED + " HD " + ChatColor.WHITE + "---------");
         p.sendMessage(ChatColor.BLACK + "https://www.dropbox.com/scl/fi/ykwfxx7lcy9e3n5cvujbc/TEADERON.zip?rlkey=zf5w2psr9jfex4i2g5wwybzso&dl=0");
         p.sendMessage(ChatColor.WHITE + "---------" + ChatColor.BLUE + ChatColor.DARK_GREEN + " SD " + ChatColor.WHITE + "---------");
-        p.sendMessage(ChatColor.BLACK + "https://www.dropbox.com/scl/fi/q7z3bqfwy7suciqu88zov/TEADERONLOW.zip?rlkey=omni19oglber05pbtypqg6ynr&dl=0");
+        p.sendMessage(ChatColor.BLACK + "https://www.dropbox.com/scl/fi/sv4etwpt1yra7th44oy4o/TEADERONLOW.zip?rlkey=58m81456d5tk03xup5ai2ayx3&dl=0");
         p.sendMessage(ChatColor.WHITE + " ");
         p.sendMessage(ChatColor.WHITE + "Версия Сервера: " + ChatColor.RED + "2.0     " + ChatColor.WHITE + "Версия Клиента: " + ChatColor.RED + "1.16.5+");
     }
